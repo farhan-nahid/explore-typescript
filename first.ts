d24180c5a0b65593f15eb6198c586e655302db75
gesora96 <nahidahmed9933@gmail.com>
2021-10-10 12:38:03 +0600

    just add a variable

--- first.ts
+++ first.ts
@@ -35,3 +35,5 @@ const outPut1: number | string = add("adam", "san");
 function doSum(num1: number, num2: number): void {
   console.log(num1 * num2);
 }
+
+const myName: string = "Farhan Ahmed Nahid";
